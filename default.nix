b406ce15d1a0f90ae3538ad0fa44595a21dbcd3d
Logan Wang <919603023@qq.com>
2025-05-20 14:42:04 +0000

    init

---
+++ default.nix
@@ -0,0 +1,40 @@
+{ stdenv, fetchzip, autoPatchelfHook, pkgs, lib}:
+
+stdenv.mkDerivation rec{
+  pname = "aptos";
+  version = "7.3.0";
+
+  src = fetchzip {
+    url = "https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v${version}/aptos-cli-${version}-Linux-x86_64.zip";
+    sha256 = "sha256-3xuxpGTua1J8mUwG6AIeiCjlfL9bUawBm7nMagnJYHI=";
+  };
+
+  nativeBuildInputs = [ autoPatchelfHook pkgs.unzip ];
+  buildInputs = [
+    pkgs.stdenv.cc.cc.lib
+    pkgs.gcc
+    pkgs.openssl_3
+    pkgs.systemd
+  ];
+  dontConfigure = true;
+  dontBuild = true;
+
+  unpackPhase = "true";
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ${src}/aptos $out/bin/
+    chmod +x $out/bin/aptos
+  '';
+  meta = with lib; {
+    description = "Command-line interface for interacting with the Aptos blockchain";
+    homepage    = "https://github.com/aptos-labs/aptos-core";
+    license     = licenses.asl20;
+    maintainers = [{
+   	name  = "WGB5445";
+    	email = "wgb98512@gmail.com";
+    	github = "WGB5445"; 
+    } ];
+    platforms   = [ "x86_64-linux" ];
+  };
+}
